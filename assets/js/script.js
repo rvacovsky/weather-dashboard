1d856129dcb5c12d50cfb8c37fcd1986ea0caaf8
Roger Vacovsky <rvacovsky@gmail.com>
2022-04-15 14:13:33 -0400

    added api to pull city data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var cityFormEl = document.querySelector("#city-search");
 var cityInputEl = document.querySelector("#city");
-var weatherContainerEl = document.querySelector("#container-lg");
+// var weatherContainerEl = document.querySelector("#container-lg");
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -8,17 +8,16 @@ var formSubmitHandler = function(event) {
     console.log(cityInputEl);
     // get value from input element
     var cityName = cityInputEl.value.trim();
-  
+
     if (cityName) {
       getWeather(cityName);
-  
-      // clear old content
-      weatherContainerEl.textContent = "";
       cityInputEl.value = "";
     } else {
       alert("Please enter a valid city name!");
     }
-  };
+};
+
+
 
 var getWeather =function(text) {
 
@@ -26,13 +25,25 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + text + "&ap
 
     fetch(apiUrl).then(function(response) {
         
-        console.log(response);
-        response.json().then(function(data) {
-        console.log(data);
-        });
+      console.log(response);
+      response.json().then(function(data) {
+      console.log(data);
+
+        const lat = data.city.coord.lat;
+        const lon = data.city.coord.lon;
+
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=51a61d96cb3c110846e5130afe5ac605")
+          .then(function (response) {
+              return response.json();
+              })
+              .then(function (data) {
+                  appendData(data);
+              // Use the console to examine the response
+              console.log(data);
+              })
+      });
     });
-};
 
+
+};        
 cityFormEl.addEventListener("submit", formSubmitHandler);
-
-
